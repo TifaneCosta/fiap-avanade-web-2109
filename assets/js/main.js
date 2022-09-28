9c8926f266df7408d5725b233431eb2c85c09c36
Tífane Costa de Sousa <t.costa.de.sousa@avanade.com>
2022-09-28 12:03:15 -0300

    Deploy

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+//trabalhando com javascript em um arquivo externo.
+
+
+
+console.log(nome, "chamando variável criada com var");
+
+
+var nome ='Fiap';
+
+
+
+
+
+escreve('exemplo de função nominal')
+//função nominal - udf
+function escreve (titulo){
+    alert(titulo);
+}
+
+
+//gravar o numero 7
+//existem 7 tipos de erros diferentes no JS
+//3 deles são os principais
+// 1 SyntaxError - escreveu algo errado
+// 2 ReferenceError - chamou alguem que não existe
+// 3 TypeError - violou uma regra
+// 4 RangeError
+// 5 EvalError
+// 6 InternalError
+// 7 URIErrors
